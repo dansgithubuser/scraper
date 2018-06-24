5da8ab607bbedbb06c633633b3133b2554f401ec
Dan <dansonlinepresence@gmail.com>
2018-06-24 19:44:41 -0400

    add some tables to scrape data into

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class PollenForecast(models.Model):
+	text=models.TextField()
+
+class AirQualityReport(models.Model):
+	text=models.TextField()
